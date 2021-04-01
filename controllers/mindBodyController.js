c2c207adfdcb461d8295e2970754e130a7f3234b
DESKTOP-027L9TR\Desarrollo1 <jcesarlopezb@gmail.com>
2021-03-31 21:27:03 -0500

    se cambio console variable rq

--- controllers/mindBodyController.js
+++ controllers/mindBodyController.js
@@ -5,10 +5,9 @@ var formidable = require('formidable');
 exports.create = async (req, res, next) => {
     // console.log(req);
     let form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
+    form.parse(req, async function (err, rq, files) {
         // `file` is the name of the <input> field of type `file`
-        console.log(files);
-        console.log(fields);
+        console.log(rq);
         try {
             const respuesta = await clienteAxios.post('/crm.contact.add.json?FIELDS[NAME]=Julio3&FIELDS[LAST_NAME]=Lopez');
             console.log(respuesta.data.result);
@@ -29,10 +28,9 @@ exports.update = async (req, res, next) => {
    // console.log(req);
 
     let form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
+    form.parse(req, function(err, rq, files) {
         // `file` is the name of the <input> field of type `file`
-        console.log(files);
-        console.log(fields);
+        console.log(rq);
         
     });
 
@@ -45,11 +43,11 @@ exports.desactivar = async (req, res, next) => {
     // console.log(req);
  
      let form = new formidable.IncomingForm();
-     form.parse(req, function(err, fields, files) {
+     form.parse(req, function(err, rq, files) {
          // `file` is the name of the <input> field of type `file`
-         //console.log(files);
-         console.log(fields);
-         console.log(fields.eventId);
+        
+         console.log(rq);
+         console.log(rq.eventId);
          
      });
  
